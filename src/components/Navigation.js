cacb4750378970105297026846052bf1f1fc6140
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-09-03 12:14:45 -0500

    Changed some CSS

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+import CoconutTree from '../assets/Coconut Tree.png'
+
+
 export default function Navigation() {
     return (
         <div className='nav'>
             <div className='div-left'>
-                L
+                <h1>Nooking<span className='lightBrown'>Way</span></h1>
+                <img src={CoconutTree}></img>
             </div>
             <div className='div-right'>
-            R
+            <Link to="/">Home</Link>
             </div>
         </div>
 
