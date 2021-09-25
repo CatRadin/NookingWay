e38abdde5ed59d5cbcb09bc102cd1db4b5f65c0e
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-09-25 13:19:10 -0500

    Some updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ export default function Home() {
     return (
         <div className='home'>
             <Nav />
-        <div className="middle"></div> 
+        <div className="middle">
+            <h1>Animal Crossing</h1>
+            </div> 
         <div className="footer"></div>   
         </div>
 
