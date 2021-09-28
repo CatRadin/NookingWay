f57ea60df0246de842289c78e0e4f4dd3370f864
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-09-28 12:14:09 -0500

    Alt Text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import Nav from './Navigation'
 import Artwork from "../assets/Artwork.png"
 import Characters from '../assets/Characters.png';
+import Clothing from '../assets/Clothing.png';
+import Critters from '../assets/Critters.png';
 import { Link } from "react-router-dom";
 
 
@@ -12,13 +14,18 @@ export default function Home() {
             <h1>Animal Crossing</h1>
             <h2>New Horizons</h2>
             <div className="links">
-
+                {/* ----------------Image Links------------------- */}
             <Link to="/artwork">
-                <img src={Artwork} />
+                <img src={Artwork} alt="Artwork"/>
             </Link>
-            
             <Link to="/characters">
-                <img src={Characters} />
+                <img src={Characters} alt="Characters" />
+            </Link>
+            <Link to="/clothing">
+                <img src={Clothing} alt="Clothing" className="clothing" />
+            </Link>
+            <Link to="/critters">
+                <img src={Critters} alt="Critters"  />
             </Link>
                
             </div>
