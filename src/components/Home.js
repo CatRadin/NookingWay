f7bb6a2263e6a615b5b77d157982d22c466ee47f
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-09-28 15:41:44 -0500

    Added the links and pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,16 @@ import Artwork from "../assets/Artwork.png"
 import Characters from '../assets/Characters.png';
 import Clothing from '../assets/Clothing.png';
 import Critters from '../assets/Critters.png';
+import DIY from '../assets/DIY.png';
+import Events from '../assets/Events.png';
+import Fossils from '../assets/Fossils.png';
+import Furniture from '../assets/Furniture.png';
+import Locations from '../assets/Locations.png';
+import Music from '../assets/Music.png';
+import Tools from '../assets/Tools.png';
+import Villagers from '../assets/Villagers.png';
+import TomNook from '../assets/TomNook.png'
+import TandT from '../assets/TandT.png'
 import { Link } from "react-router-dom";
 
 
@@ -13,8 +23,10 @@ export default function Home() {
         <div className="middle">
             <h1>Animal Crossing</h1>
             <h2>New Horizons</h2>
+            <img src={TomNook} alt="Toom Nook" className="TomNook" />
+            <img src={TandT} alt="Timmy And Tommy" className="TandT" />
             <div className="links">
-                {/* ----------------Image Links------------------- */}
+                {/* ----------------Image Links # 1------------------- */}
             <Link to="/artwork">
                 <img src={Artwork} alt="Artwork"/>
             </Link>
@@ -26,12 +38,41 @@ export default function Home() {
             </Link>
             <Link to="/critters">
                 <img src={Critters} alt="Critters"  />
+            </Link>   
+            </div>
+            {/* ----------------Image Links # 2------------------- */}
+            <div className="links">
+             <Link to="/diy">
+                <img src={DIY} alt="DIY"  />
             </Link>
-               
+            <Link to="/events">
+                <img src={Events} alt="DIY"  />
+            </Link>
+            <Link to="/fossils">
+                <img src={Fossils} alt="fossils"  />
+            </Link>
+            <Link to="/furniture">
+                <img src={Furniture} alt="furniture"  />
+            </Link>
+            </div>
+            {/* ----------------Image Links # 3------------------- */}
+            <div className="links">
+            <Link to="/locations">
+                <img src={Locations} alt="locations" className="clothing" />
+            </Link> 
+            <Link to="/music">
+                <img src={Music} alt="Music" className="clothing" />
+            </Link> 
+            <Link to="/tools">
+                <img src={Tools} alt="Tools" className="clothing" />
+            </Link>
+            <Link to="/villagers">
+                <img src={Villagers} alt="Villagers" className="clothing" />
+            </Link>  
             </div>
             </div> 
             
-        <div className="footer">dfdsfds</div>   
+        <div className="footer"></div>   
         </div>
 
     );
