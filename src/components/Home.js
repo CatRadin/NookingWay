532127fa38b37fb85eab17b6adc0643dea748927
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-09-28 11:51:57 -0500

    Build

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import Nav from './Navigation'
+import Artwork from "../assets/Artwork.png"
+import Characters from '../assets/Characters.png';
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
@@ -8,7 +11,19 @@ export default function Home() {
         <div className="middle">
             <h1>Animal Crossing</h1>
             <h2>New Horizons</h2>
+            <div className="links">
+
+            <Link to="/artwork">
+                <img src={Artwork} />
+            </Link>
+            
+            <Link to="/characters">
+                <img src={Characters} />
+            </Link>
+               
+            </div>
             </div> 
+            
         <div className="footer">dfdsfds</div>   
         </div>
 
